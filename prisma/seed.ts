f0971fd0813dd900026d11b9b0d9d680545ec08a
Sergey <spushkovskiy@gmail.com>
2024-04-02 21:31:49 +0300

    Updated seed and configs

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -5,19 +5,43 @@ const prisma = new PrismaClient();
 async function main() {
   const salt = await genSalt();
   const hashedPassword = await hash('12345', salt);
+  const existingCategories = await prisma.category.count();
 
-  await prisma.user.upsert({
-    where: { email: 'admin@mail.com' },
-    update: {},
-    create: {
-      email: 'admin@mail.com',
-      firstName: 'Admin',
-      lastName: 'Admin',
-      phone: '1234567890',
-      password: hashedPassword,
-      role: $Enums.Role.ADMIN,
-    },
-  });
+  try {
+    await prisma.user.upsert({
+      where: { email: 'admin@mail.com' },
+      update: {},
+      create: {
+        email: 'admin@mail.com',
+        firstName: 'Admin',
+        lastName: 'Admin',
+        phone: '1234567890',
+        password: hashedPassword,
+        role: $Enums.Role.ADMIN,
+      },
+    });
+
+    if (existingCategories === 0) {
+      await prisma.category.createMany({
+        data: [
+          {
+            name: 'Bathing & Hygiene',
+          },
+          {
+            name: 'Skin Care',
+          },
+          {
+            name: 'Health Care',
+          },
+          {
+            name: 'Beauty Products',
+          },
+        ],
+      });
+    }
+  } catch (error) {
+    console.debug('Error while seeding', error);
+  }
 }
 
 main()
--- src/file/file.module.ts
+++ src/file/file.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MulterModule } from '@nestjs/platform-express';
 import { FileService } from './file.service';
+import { path } from 'app-root-path';
 
 @Module({
   imports: [
-    MulterModule.register({ dest: './uploads' }), // Configure file storage location
+    MulterModule.register({ dest: `${path}/uploads` }), // Configure file storage location
   ],
   providers: [FileService],
   exports: [FileService],
